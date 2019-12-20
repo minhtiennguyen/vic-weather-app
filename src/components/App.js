f6bb6bd08d5995446df0a6721a2f74e8e7741875
Minh Tien Nguyen <tien.minh@hubcba.com>
2019-12-20 16:32:05 +1100

    init

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='app'>
+      <header className='header'>Header</header>
+      <div className='main'>
+        Main
+      </div>
+    </div>
+  );
+}
+
+export default App;
