b16e85d457bbbebe6aa770ff62692c3c3474de43
Minh Tien Nguyen <tien.minh@hubcba.com>
2019-12-21 18:02:27 +1100

    add gh-pages and disbale devtool on production

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
   apiKey: process.env.REACT_APP_WEATHER_API_KEY,
-  apiUrl: 'http://api.openweathermap.org/data/2.5/weather',
-  imgUrl: 'http://openweathermap.org/img/wn'
+  apiUrl: 'https://api.openweathermap.org/data/2.5/weather',
+  imgUrl: 'https://openweathermap.org/img/wn'
 }
 
 export default config;
