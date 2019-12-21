366d014537795bf58d4f4e05d6402e7b110175e7
Minh Tien Nguyen <tien.minh@hubcba.com>
2019-12-21 17:39:42 +1100

    Updates

---
+++ precache-manifest.e9c5d493f213ef470a453b7a5e1026de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fc0d68c50a206ee31b6cbc87ae5a273",
+    "url": "/minhtiennguyen/vic-weather-app/index.html"
+  },
+  {
+    "revision": "91e6227df1ddd86e23bd",
+    "url": "/minhtiennguyen/vic-weather-app/static/css/main.9e63278e.chunk.css"
+  },
+  {
+    "revision": "b7b63a63894d39d8445c",
+    "url": "/minhtiennguyen/vic-weather-app/static/js/2.a9edd269.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/minhtiennguyen/vic-weather-app/static/js/2.a9edd269.chunk.js.LICENSE"
+  },
+  {
+    "revision": "91e6227df1ddd86e23bd",
+    "url": "/minhtiennguyen/vic-weather-app/static/js/main.8cd49826.chunk.js"
+  },
+  {
+    "revision": "96d244df371a3ae5f430",
+    "url": "/minhtiennguyen/vic-weather-app/static/js/runtime-main.81d0a152.js"
+  }
+]);
