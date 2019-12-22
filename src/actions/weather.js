a611c05b199042108b92832b2a1a29bfe853b7d1
Minh Tien Nguyen <tien.minh@hubcba.com>
2019-12-22 19:48:02 +1100

    fix bugs

--- src/actions/weather.js
+++ src/actions/weather.js
@@ -6,8 +6,8 @@ const fetchWeatherSuccess = createAction(FETCH_WEATHER_SUCCESS);
 const fetchWeatherFailed = createAction(FETCH_WEATHER_FAILED);
 const selectMainCity = createAction('SELECT_MAIN_CITY');
 
-const fetchWeather = (cityId, unit) => dispatch => {
-  const url = `${config.apiUrl}?id=${cityId}&APPID=${config.apiKey}&units=${unit}`;
+const fetchWeather = (cityId) => dispatch => {
+  const url = `${config.apiUrl}?id=${cityId}&APPID=${config.apiKey}&units=metric`;
   dispatch({ type: FETCH_WEATHER });
   return fetch(url)
     .then(res => res.json())
